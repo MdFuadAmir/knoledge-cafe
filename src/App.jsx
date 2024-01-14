256935e4b7f544275399dcc9588649745331babf
Md Fuad Amir <mdfuadamir@gmail.com>
2024-01-15 01:18:48 +0600

    added Blogs,Blog,Bookmarks components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Blogs from './componentes/Blogs/Blogs'
+import Bookmarks from './componentes/Bookmarks/Bookmarks'
 import Header from './componentes/header/Header'
 
 function App() {
@@ -8,7 +9,11 @@ function App() {
   return (
     <>
     <Header></Header>
+    <div className='md:flex my-12'>
     <Blogs></Blogs>
+    <Bookmarks></Bookmarks>
+
+    </div>
 
       
         
